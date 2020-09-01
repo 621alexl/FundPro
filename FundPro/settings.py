479ea67f49db42e3b7eb3ba6f13310693b2e6fc6
621alexl <621alexl@gmail.com>
2020-09-01 02:20:12 -0400

    Heroku changes

--- FundPro/settings.py
+++ FundPro/settings.py
@@ -86,8 +86,13 @@ WSGI_APPLICATION = 'FundPro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {    
+    'default': {        
+        'ENGINE': 'django.db.backends.sqlite3',        
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),    
+    }
+}
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
